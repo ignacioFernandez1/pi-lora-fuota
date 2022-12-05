df746b525f2c9bf4bd6b402f2a5bac0b3c5f0e77
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2022-12-05 10:42:50 -0300

    device y pygate codigo actualizado europa

--- Devices/main.py
+++ Devices/main.py
@@ -3,15 +3,33 @@ import socket
 import time
 import binascii
 import pycom
+import struct
 
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868, device_class=LoRa.CLASS_C, adr=True)
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868, device_class=LoRa.CLASS_C)
 
-dev_eui = binascii.unhexlify('c5c65c3303799380'.replace(' ', ''))
+def lora_cb(lora):
+    events = lora.events()
+    if events & LoRa.RX_PACKET_EVENT:
+        if s is not None:
+            frame, port = s.recvfrom(512) # longuest frame is +-220
+            print(port, frame)
+    if events & LoRa.TX_PACKET_EVENT:
+        print("tx_time_on_air: {} ms @dr {}", lora.stats().tx_time_on_air, lora.stats().sftx)
+
+dev_eui = binascii.unhexlify('c5c65c3303799382'.replace(' ', ''))
 app_eui = binascii.unhexlify('00 00 00 00 00 00 00 00'.replace(' ', ''))
 app_key = binascii.unhexlify('80 4d 8b 48 50 98 62 be 9d 84 a7 db ae 7c 4a 88'.replace(' ', ''))
-lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
+mcAddr = struct.unpack(">l", binascii.unhexlify('0053cd8f'))[0]
+mcNwkKey = binascii.unhexlify('F9CEFC8DC156D1CA0C61F664B1C84725')
+mcAppKey = binascii.unhexlify('01CF92D6F28A7F7A193647739059278C')
+print('joining multicast')
+lora.join_multicast_group(mcAddr, mcNwkKey, mcAppKey)
+print('joined multicast')
+
+# lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 print("devEUI {}".format(binascii.hexlify(lora.mac())))
+
 
 pycom.heartbeat(False)
 pycom.rgbled(0x111111)
@@ -23,8 +41,9 @@ while not lora.has_joined():
     print('.', end='')
 print('Network joined!')
 
-
-print(lora.stats().tx_frequency)
+lora.callback(trigger=( LoRa.RX_PACKET_EVENT |
+                        LoRa.TX_PACKET_EVENT |
+                        LoRa.TX_FAILED_EVENT  ), handler=lora_cb)
 
 send_interval = 1
 seconds = 30
@@ -50,40 +69,3 @@ while True:
     print(" Send data success....Entering sleep mode")
     pycom.heartbeat(True)
     time.sleep(send_interval * seconds)
-
-# s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-# # s.bind(5)
-# s.setsockopt(socket.SOL_LORA, socket.SO_DR, 4)
-# s.setsockopt(socket.SOL_LORA,  socket.SO_CONFIRMED,  True)
-#
-# cpt = 1
-#
-# while True:
-#    pycom.rgbled(0x110000)
-#    s.setblocking(True)
-#    s.settimeout(30)
-#
-# # for testing new code, remove try/except
-#
-#    try:
-#        print('sending', cpt)
-#        s.send('Hello LoRa {}'.format(cpt))
-#    except:
-#        print ('timeout in sending')
-#
-#    pycom.rgbled(0x001100)
-#
-#    try:
-#        data = s.recv(64)
-#
-#        print(data)
-#        pycom.rgbled(0x000011)
-#    except:
-#        print ('timeout in receive')
-#        pycom.rgbled(0x000000)
-#
-#
-#    s.setblocking(False)
-#    time.sleep (30)
-#
-#    cpt += 1
